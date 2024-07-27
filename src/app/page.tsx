a4a9ec2cc1c82dbf3a1bb980796d17c10e1ae365
Mukesh Kannan <mukeshkannan311@gmail.com>
2024-07-27 22:57:19 +0530

    [FIX]: ✨ window not defined build error 🍉

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 "use client";
+
 import useCanvas from "@/hooks/useCanvas";
-import { useState } from 'react'
+import { Point } from "@/utils/type";
+import { useRef, useState } from "react";
 
 export default function Home() {
-  const [isFloodFill, setIsFloodFill] = useState(false)
+  const [isFloodFill, setIsFloodFill] = useState(false);
   const { canvasRef } = useCanvas(8, 8, 50, isFloodFill);
+  const windowDim = useRef<Point>()
+
+  if (typeof window !== "undefined") {
+    windowDim.current = {x: window.innerWidth, y: window.innerHeight}
+  }
 
   return (
     <main className="h-screen w-full bg-[#F3EEE3] relative flex items-center justify-center">
@@ -14,13 +21,13 @@ export default function Home() {
         <h1 className="cursor-pointer hover:text-[#B7A7F0]">HELP</h1>
       </nav>
 
-      <canvas
-        ref={canvasRef}
-        height={window.innerHeight}
-        width={window.innerWidth}
-        className="bg-black"
-      ></canvas>
-      <button className="text-white absolute bottom-0 right-0" onClick={() => setIsFloodFill(!isFloodFill)}>{isFloodFill ? 'FILL MODE' : 'DRAW MODE'}</button>
+      <canvas ref={canvasRef} height={windowDim.current?.x || 0} width={windowDim.current?.y || 0}></canvas>
+      <button
+        className="text-white absolute bottom-0 right-0"
+        onClick={() => setIsFloodFill(!isFloodFill)}
+      >
+        {isFloodFill ? "FILL MODE" : "DRAW MODE"}
+      </button>
 
       <section className="h-24 w-1/3 bg-[#D9D0BE] absolute bottom-0 left-0 right-0 mx-auto z-[999]"></section>
     </main>
